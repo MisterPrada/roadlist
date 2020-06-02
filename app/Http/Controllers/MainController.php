8515fccb9519f09f4762f935240585aca718e191
MisterPrada <lesha.skorpion@gmail.com>
2020-06-03 01:54:43 +0300

    RoadList v1.10.1 - Фикс архива для яндекса

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -148,7 +148,7 @@ class MainController extends Controller
 
 
             // выставляем значения на первом листе
-            $activeSheet1->setCellValue('DQ53', '' . $price); // Выставляем цену
+            $activeSheet1->setCellValue('DQ53', '999'); // Выставляем цену
             $activeSheet1->setCellValue('DP55', $request->tax_user); // Выставляем таксировщика
             $activeSheet1->setCellValue('FG55', $request->exp_user); // Выставляем начальника эксплуатации
             $activeSheet1->setCellValue('DP46', $item->n1 . ' ' . $item->n2); // Выставляем номер путевого листа
